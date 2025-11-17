9029991cdab97e4bacc2690a3f5160604afdf075
a48161845-jpg <a48161845@gmail.com>
2025-11-17 23:34:39 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,8 +23,8 @@ logger = logging.getLogger(__name__)
 CONFIG = {
     'TOKEN': os.environ.get('BOT_TOKEN'),
     'SPREADSHEET_URL': "https://docs.google.com/spreadsheets/d/1o_qYVyRkbQ-bw5f9RwEm4ThYEGltHCfeLLf7BgPgGmI/edit?usp=drivesdk",
-    'CHAT_ID': "-1003452972632",
-    'THREAD_ID': 25,  # Укажите существующий thread или оставьте None
+    'CHAT_ID': "-1002124864225",
+    'THREAD_ID': 16232,  # Укажите существующий thread или оставьте None
     'TIMEZONE_OFFSET': datetime.timedelta(hours=3),
     'CACHE_FILE': 'birthday_cache.json',
     'CACHE_EXPIRY': 300,
